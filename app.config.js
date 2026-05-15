034f17a2b00771c3e6b6da098bd5e52a027aead6
Nicklas <fredriksson.n@gmail.com>
2026-05-15 23:34:17 +0200

    Update Expo app config and branding assets

---
+++ app.config.js
@@ -0,0 +1,33 @@
+export default {
+  expo: {
+    name: "SSKHBG",
+    slug: "sskhbg",
+    owner: "sskhbg",
+    scheme: "sskhbg",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#0f1115"
+    },
+    plugins: ["expo-router", "expo-sqlite"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.nicklas.sskhbg",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    android: {
+      package: "com.nicklas.sskhbg"
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "92b8c339-25bb-4589-9ed1-bc0bb67e28a1"
+      }
+    }
+  }
+};
